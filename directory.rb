e2d873d6d6c85d590feb3892e7596979e52278af
Freddie <fredjlawson@gmail.com>
2018-08-01 12:10:44 -0400

    working, about to start experimenting

--- directory.rb
+++ directory.rb
@@ -4,19 +4,22 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
+  puts "Now please enter the cohort"
+  cohort = gets.chomp.to_sym
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  until name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
+    puts "Now please enter the cohort"
+    cohort = gets.chomp.to_sym
   end
   # return the array of students
   students
 end
-
 
 
 def print_header
@@ -24,13 +27,19 @@ def print_header
   puts "-------------"
 end
 
-def print_shorter_than_12(students)
-  students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+def print_array(array)
+  print array
+end
+
+
+def print_formatted(students)
+  counter = 0
+  until students[counter] == nil do
+    puts "#{counter + 1}. #{students[counter][:name].center(15)} (#{students[counter][:cohort]} cohort)"
+    counter += 1
   end
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
@@ -38,5 +47,6 @@ end
 
 students = input_students
 print_header
-print_shorter_than_12(students)
-print_footer(students)
+print_array(students)
+# print_formatted(students)
+# print_footer(students)
---
+++ notes.rb
@@ -0,0 +1,7 @@
+hash = {:name => "freddie"}
+
+# puts hash[:name][0]
+
+name = "freddie"
+
+puts name.center(30)
