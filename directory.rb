0c45ca1b86e19939d9fb6d3aa4b0088f12e4a993
Freddie <fredjlawson@gmail.com>
2018-08-02 10:23:47 -0400

    added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,44 +1,46 @@
-def input_students
 
-  # create an empty array
+def cohort_getter
   possible_cohorts = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  puts "Please enter the cohort of the student"
+  while true do
+    cohort = gets.chomp.capitalize.to_sym
+    if possible_cohorts.include?(cohort.to_s)
+      break
+    else
+      puts "Cohort not recognised, please input again"
+    end
+  end
+  cohort # Returning the spell checked cohort
+end
+
+
+def input_students # This will provide an array of students and their data
   students = []
-  cohort = ""
-  # get the first name
   while true do
     puts "Please enter the name of the student"
     name = gets.chomp
     if name == ""
       break
     end
-
-    puts "Please enter the cohort of the student"
-    while true do
-      cohort = gets.chomp.capitalize.to_sym
-      if possible_cohorts.include?(cohort.to_s)
-        break
-      else
-        puts "Cohort not recognised, please input again"
-      end
-    end
-
     puts "please enter the age of the student"
     age = gets.chomp.to_i
 
-    # add the student hash to the array
-    students << {name: name, cohort: cohort, age: age}
+    # add the student hash to the array (we'll get the cohort in a second)
+    students << {name: name, cohort: cohort_getter, age: age}
     puts "Now we have #{students.count} students"
     puts "--------"
-
   end
   # return the array of students
   students
 end
 
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 
 def print(students)
   students.each do |student|
@@ -46,12 +48,27 @@ def print(students)
   end
 end
 
+
 def print_footer(students)
+  puts "---------"
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
+def print_by_cohort(students, cohort) # Passing in the students array
+puts "Printing students in the #{cohort} cohort"
+puts "---------"
+  students.each do |student|
+    if student[:cohort] == cohort.to_sym
+      puts "#{student[:name]}, #{student[:age]}"
+    end
+
+  end
+end
+
+students = input_students # setting the variable students equal to the output of our method
 #nothing happens until we call the methods
 print_header
-print(students)
+puts "Printing by cohort - input a cohort"
+cohort_selection = gets.chomp.capitalize
+print_by_cohort(students, cohort_selection)
 print_footer(students)
---
+++ fixing_typos.rb
@@ -0,0 +1,68 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  if students.empty?
+    puts "No students to print!"
+  else
+    students.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+
+=begin
+print_header # This works fine
+print(students) # This is working now, "print" is a bad method name, basically
+print_footer(students)
+=end
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't understand, please try again"
+      end
+  end
+end
+
+interactive_menu
